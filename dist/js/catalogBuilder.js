89e38960918711f102c6b7981ab2313f89f75d50
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-11-16 09:47:46 -0300

    feat: Criação da filtragem por cores

--- dist/js/catalogBuilder.js
+++ dist/js/catalogBuilder.js
@@ -1,8 +1,8 @@
 import { createProductElement, createProductListRowElement } from "./productElementBuilder.js";
 
 
-export default async function buildCatalog(sizeFilter = []) {
-    console.log(sizeFilter);
+export default async function buildCatalog(filters = { 'size': [], 'color': [] }) {
+    console.log(filters);
 
     const query = await fetch('http://localhost:5000/products', {
         method: 'GET'
@@ -10,27 +10,58 @@ export default async function buildCatalog(sizeFilter = []) {
     const products = await query.json();
 
     let productListElement = document.getElementById('product-list');
+    clearProductList(productListElement);
+
+    let filteredProducts;
+    if (filters['size'].length > 0 && filters['color'].length > 0) {
+        filteredProducts = filterBySize(filters['size'], products);
+        filteredProducts = filterByColor(filters['color'], filteredProducts);
+        appendCatalogRows(productListElement, filteredProducts);
+    } else if (filters['size'].length > 0 && filters['color'].length === 0) {
+        filteredProducts = filterBySize(filters['size'], products);
+        appendCatalogRows(productListElement, filteredProducts);
+    } else if (filters['size'].length === 0 && filters['color'].length > 0) {
+        filteredProducts = filterByColor(filters['color'], products);
+        appendCatalogRows(productListElement, filteredProducts);
+    } else {
+        appendCatalogRows(productListElement, products);
+    }
+}
+
+function clearProductList(productListElement) {
     if (productListElement.children.length > 0) {
         while (productListElement.firstChild) {
             productListElement.removeChild(productListElement.lastChild);
         }
     }
+}
 
-    if (sizeFilter.length > 0) {
-        let filteredProducts = [];
-        sizeFilter.forEach(size => {
-            products.forEach(product => {
-                if (product.size.includes(size)) {
-                    if (!filteredProducts.includes(product)) {
-                        filteredProducts.push(product);
-                    }
+function filterBySize(filters, products) {
+    let filteredProducts = [];
+    filters.forEach(size => {
+        products.forEach(product => {
+            if (product.size.includes(size)) {
+                if (!filteredProducts.includes(product)) {
+                    filteredProducts.push(product);
                 }
-            });
+            }
         });
-        appendCatalogRows(productListElement, filteredProducts);
-    } else {
-        appendCatalogRows(productListElement, products);
-    }
+    });
+    return filteredProducts;
+}
+
+function filterByColor(filters, products) {
+    let filteredProducts = [];
+    filters.forEach(color => {
+        products.forEach(product => {
+            if (product.color.includes(color)) {
+                if (!filteredProducts.includes(product)) {
+                    filteredProducts.push(product);
+                }
+            }
+        });
+    });
+    return filteredProducts;
 }
 
 function appendCatalogRows(productListElement, products) {
--- dist/js/sizeFilter.js
+++ dist/js/sizeFilter.js
@@ -3,18 +3,40 @@ import buildCatalog from "./catalogBuilder.js";
 let sizeFilters = document.getElementsByClassName('size-filter-option');
 sizeFilters = Array.from(sizeFilters);
 
-let requiredFilters = [];
+let colorFilters = document.getElementsByClassName('color-filter-option');
+colorFilters = Array.from(colorFilters);
+
+let requiredFilters = {
+    'size': [],
+    'color': []
+};
 
 sizeFilters.forEach(filter => {
     filter.addEventListener("click", (event) => {
         const input = filter.firstElementChild;
         const inputValue = input.value;
         if (event.target.localName === 'input') {
-            if (!requiredFilters.includes(inputValue)) {
-                requiredFilters.push(inputValue);
+            if (!requiredFilters['size'].includes(inputValue)) {
+                requiredFilters['size'].push(inputValue);
             } else {
-                const index = requiredFilters.indexOf(inputValue);
-                requiredFilters.splice(index, 1);
+                const index = requiredFilters['size'].indexOf(inputValue);
+                requiredFilters['size'].splice(index, 1);
+            }
+            buildCatalog(requiredFilters);
+        }
+    });
+});
+
+colorFilters.forEach(filter => {
+    filter.addEventListener("click", (event) => {
+        const input = filter.firstElementChild;
+        const inputValue = input.value;
+        if (event.target.localName === 'input') {
+            if (!requiredFilters['color'].includes(inputValue)) {
+                requiredFilters['color'].push(inputValue);
+            } else {
+                const index = requiredFilters['color'].indexOf(inputValue);
+                requiredFilters['color'].splice(index, 1);
             }
             buildCatalog(requiredFilters);
         }
